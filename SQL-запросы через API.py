2ffa966bcfb09f5980113d19c07175fe1ac6773c
PixelAndy <zpixel.andy@gmail.com>
2019-09-17 14:49:34 +0300

    need fix

--- SQL-запросы через API.py
+++ SQL-запросы через API.py
@@ -2,17 +2,18 @@ import sqlite3
 
 
 with sqlite3.connect(r'./datumproject/db.sqlite3') as conn:
-    conn.enable_load_extension(True)
-    conn.execute("SELECT load_extension('mod_spatialite.dll')")
-    conn.execute("SELECT InitSpatialMetaData()")
+    #TODO: need fix load extension problem
+    # conn.enable_load_extension(True)
+    # conn.execute("SELECT load_extension('mod_spatialite.dll')")
+    # conn.execute("SELECT InitSpatialMetaData()")
 
     c = conn.cursor()
 
     c.execute("insert into DatumApp_type ( name, description ) values ( 'education', 'Education' )")
     c.execute("insert into DatumApp_type ( name, description ) values ( 'liesure', 'Liesure' )")
     c.execute("insert into DatumApp_type ( name, description ) values ( 'sport', 'Sport' )")
-    c.execute("""insert into DatumApp_object (id, name, description, type_id, geo_coord) 
-                 values (1, 'school' , 'school description', 1, GeomFromText('POINT(1.01 2.02)', 4326))""")
+    # c.execute("""insert into DatumApp_object (id, name, description, type_id, geo_coord)
+    #              values (1, 'school' , 'school description', 1, GeomFromText('POINT(1.01 2.02)', 4326))""")
     c.execute('select * from DatumApp_object')
     conn.commit()
     print('done!\n', c.fetchall())
